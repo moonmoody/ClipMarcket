d293f0032f0b7fb750a87a21ab154c4e1fa61277
dck <dckim15@naver.com>
2025-06-27 16:26:37 +0900

    [수정] virginia에 추가

--- virginia/main.tf
+++ virginia/main.tf
@@ -3,6 +3,7 @@ module "verginia_vpc" {
   pjt_name = var.virginia_pjt_name
   vpc_cidr = var.virginia_vpc_cidr
   subnets = var.virginia_subnets
+  nat_gw_azs = var.virginia_nat_gw_azs
 
   providers = {
     aws = aws.virginia
