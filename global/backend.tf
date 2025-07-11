e10f3c3cb3ac1b75d8fa034fc406b12452ff0d23
Seunggyun Moon <moonsg6696@gmail.com>
2025-07-11 17:40:45 +0900

    수정

--- global/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "clipmarket-terraform-state"
-    key            = "global/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
