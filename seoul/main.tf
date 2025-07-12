0b049d39b961003cdfb9be678371b2b8d0a0ce42
dckim15 <dckim3515@gmail.com>
2025-07-12 10:53:50 +0900

    [수정] global data

--- seoul/main.tf
+++ seoul/main.tf
@@ -1,13 +1,11 @@
-# data "terraform_remote_state" "global_outputs" {
-#   backend = "s3" # global 환경이 사용하는 백엔드 타입 (예: s3)
-
-#   config = {
-#     # global 환경의 상태 파일 정보를 정확히 입력합니다.
-#     bucket = "my-company-tfstate-bucket" # 상태 파일이 저장된 S3 버킷
-#     key    = "global/terraform.tfstate"  # global 환경의 상태 파일 경로
-#     region = "ap-northeast-2"            # S3 버킷이 있는 리전
-#   }
-# }
+data "terraform_remote_state" "global_outputs" {
+  backend = "s3"
+  config = {
+    bucket = "clipmarket-terraform-state" # 상태 파일이 저장된 S3 버킷
+    key    = "global/terraform.tfstate"  # global 환경의 상태 파일 경로
+    region = "ap-northeast-2"            # S3 버킷이 있는 리전
+  }
+}
 
 module "seoul_vpc" {
   source     = "../vpc_module"
@@ -31,8 +29,8 @@ module "seoul_instance" {
   subnets                               = var.seoul_subnets
   ingress_rule_config                   = var.seoul_ingress_rule_config
   egress_rule_config                    = var.seoul_egress_rule_config
-  # ssm_instance_profile_name_from_global = data.terraform_remote_state.global_outputs.outputs.ssm_instance_profile_name
-  ssm_instance_profile_name_from_global = "bastion-ssm-instance-profile"
+  ssm_instance_profile_name_from_global = data.terraform_remote_state.global_outputs.outputs.ssm_instance_profile_name
+  # ssm_instance_profile_name_from_global = "bastion-ssm-instance-profile"
   pub_asg_config                        = var.seoul_pub_asg_config
 
   providers = {
